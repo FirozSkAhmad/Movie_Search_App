23175ed12ee3d09a09970cae8e46e86653878bbf
FirozSkAhmad <you@example.com>
2023-05-12 22:05:20 +0530

    changed some css

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,9 +5,11 @@ const MovieCard = (props) => {
     const { Title, Year, Poster } = data
 
     return (
-        <div className="w-64 h-80 flex flex-col items-center p-3 rounded-md bg-gray-400">
-            <img className="w-52 h-52 my-2 rounded-md" alt="movie-poster" src={Poster} />
-            <div>
+        <div className="w-64 h-80 flex flex-col p-3 rounded-md bg-gray-400">
+            <div className="flex justify-center">
+                <img className="w-52 h-52 my-2 rounded-md" alt="movie-poster" src={Poster} />
+            </div>
+            <div className="ml-3">
                 <h1 className="my-1"><span className="font-medium">Movie Name : </span>{Title.split(" ").length > 2 ? Title.split(" ").slice(0, 2).join(" ") + "..." : Title.split(" ").slice(0, 2).join(" ")}</h1>
                 <h2 className="my-1"><span className="font-medium">Release Date : </span>{Year}</h2>
             </div>
