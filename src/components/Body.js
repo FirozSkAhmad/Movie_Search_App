060a1a51859f0e68e12d007dc21083c420ca6f55
FirozSkAhmad <you@example.com>
2023-05-12 22:00:11 +0530

    added empty movie name component

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,25 +4,29 @@ import { Link } from 'react-router-dom'
 import Shimmer from "./Shimmer"
 import NoMovies from "./NoMovies"
 import useMoviesData from '../../utils/useMoviesData'
+import EmptyName from "./EmptyName"
 
 
 const Body = () => {
 
-    const [name, setName,data, setData, getData] = useMoviesData()
+    const [name, setName, data, setData, getData] = useMoviesData()
+
+    const [nameHolder, setNameHolder] = useState(name)
 
     function searchHandler() {
         setData([])
-        getData(name)
+        setName(nameHolder)
+        getData(nameHolder)
     }
 
     return (
         <div className="my-5">
             <div className="flex justify-center items-center m-10 gap-3">
-                <input className="border-2 border-blue-500 p-2 rounded-md focus:border-blue-950" value={name} type="text" placeholder="Movie Name" onChange={(e) => setName(e.target.value)}></input>
+                <input className="border-2 border-blue-500 p-2 rounded-md focus:border-blue-950" value={nameHolder} type="text" placeholder="Movie Name" onChange={(e) => setNameHolder(e.target.value)}></input>
                 <button className="bg-blue-500 p-2 rounded-md text-white font-medium" onClick={searchHandler}>Search</button>
             </div>
             <div className="flex flex-wrap justify-center gap-5">
-                {(data) ? (data.length > 0) ? data.map((movieData) => <Link key={movieData.imdbID} to={`moviedata/${movieData.imdbID}`}><MovieCard data={movieData} /></Link>) : <Shimmer /> : <NoMovies />}
+                {(data) ? (data.length > 0) ? data.map((movieData) => <Link key={movieData.imdbID} to={`moviedata/${movieData.imdbID}`}><MovieCard data={movieData} /></Link>) : <Shimmer /> : (name) ? <NoMovies /> : <EmptyName />}
             </div>
         </div>
     )
---
+++ src/components/EmptyName.js
@@ -0,0 +1,7 @@
+const EmptyName=()=>{
+    return(
+        <h1 className="flex h-36 justify-center items-center text-lg text-red-600 font-bold">Please fill the movie name</h1>
+    )
+}
+
+export default EmptyName
